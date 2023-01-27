ed68ec79e39b41aa29130f6e0cd1146f09abd81f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-27 14:19:33 +0800

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray-100": "#8c8c8c",
+        "dark-gray-200": "#696969",
+      },
+      fontFamily: {
+        "alata": ["Alata", "sans-serif"],
+        "josefin": ["Josefin Sans", "sans-serif"],
+      },
+      fontSize: {
+        "15px": "0.9375rem",
+      }
+    },
   },
   plugins: [],
 }
