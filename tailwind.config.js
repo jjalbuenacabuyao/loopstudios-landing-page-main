764d4c5ae115b0712208d2235808359bbdb9b42e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-28 13:35:39 +0800

    added custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
       },
       fontSize: {
         "15px": "0.9375rem",
+      },
+      padding: {
+        "24-48": "clamp(1.5rem, 0.069rem + 6.11vw, 3rem)",
+        "12-20": "clamp(0.75rem, -0.20rem + 4.07vw, 1.75rem)",
+      },
+      margin: {
+        "24-48": "clamp(1.5rem, 0.069rem + 6.11vw, 3rem)",
       }
     },
   },
