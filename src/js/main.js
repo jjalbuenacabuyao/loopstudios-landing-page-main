9abd186b2285bf329dc9b654c7a20740573474d7
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-28 13:37:11 +0800

    prevent scrolling when nav is opened

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,18 @@ const menuIcon = document.querySelector("[data-menuIcon]");
 const closeIcon = document.querySelector("[data-closeIcon]");
 
 navToggler.addEventListener("click", () => {
-  console.log("clicked");
   const isVisible = navigation.getAttribute("data-visible") === "true"
   menuIcon.classList.toggle("hidden")
   if (!isVisible){
     closeIcon.classList.replace("hidden", "block")
     navigation.classList.replace("scale-0", "scale-1")
     navigation.setAttribute("data-visible", "true")
+    document.body.style.overflowY = "hidden"
   } else {
     closeIcon.classList.replace("block", "hidden")
     navigation.classList.replace("scale-1", "scale-0")
     navigation.setAttribute("data-visible", "flase")
+    document.body.style.overflowY = "auto"
   }
   
 })
