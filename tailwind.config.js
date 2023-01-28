9198d7ff64914c143df2a77cfb5f2502da37d3fa
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-28 14:24:32 +0800

    added custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,15 @@ module.exports = {
       padding: {
         "24-48": "clamp(1.5rem, 0.069rem + 6.11vw, 3rem)",
         "12-20": "clamp(0.75rem, -0.20rem + 4.07vw, 1.75rem)",
+        "48-164": "clamp(3rem, -5.29rem + 17.26vw, 10.25rem)",
+        "80-160": "clamp(5rem, -0.71rem + 11.9vw, 10rem)",
+        "80-95": "clamp(5rem, 3.93rem + 2.23vw, 5.9375rem)",
+        "80-184": "clamp(5rem, -2.43rem + 15.48vw, 11.5rem)",
       },
       margin: {
         "24-48": "clamp(1.5rem, 0.069rem + 6.11vw, 3rem)",
+        "48-164": "clamp(3rem, -5.29rem + 17.26vw, 10.25rem)",
+        "80-95": "clamp(5rem, 3.93rem + 2.23vw, 5.9375rem)",
       }
     },
   },
